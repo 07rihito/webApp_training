9a0f6f94f9995bd3d210d4a81c8dbab7a48faf7f
07rihito <onashu.ganbaruzoi@gmail.com>
2021-10-10 03:14:41 +0900

    [mody]add routing for userList and errorPage

--- web/app/index.py
+++ web/app/index.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
@@ -8,7 +7,17 @@ from flask import abort
 # from urllib.parse import urlparse
 import mysql.connector
 
-app = Flask(__name__)
+from dbModels import Users
+from application import app
+from application import db
+
+## Create Flask Application
+#app = Flask(__name__)
+## Set Config Class
+#app.config.from_object('config.Config')
+## Set DB
+#db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
@@ -20,6 +29,18 @@ def about():
   props = {'title': 'About this app'}
   return render_template('about.html', props=props)
 
+@app.route('/userList')
+def userlist():
+  try:
+    users = Users.query.all()
+    props = {'title': 'userList'}
+    # TODO db接続テスト用に登録user一覧ページを作成する．将来的には管理者しか見れないように
+    return render_template('userList.html', users=users, props=props)
+  except Exception as e:
+    props = {'title': 'error'}
+    props['errorMsg'] = e
+    return render_template('errorPage.html', props=props)
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=False)
