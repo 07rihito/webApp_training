c8fc2255fc8551ac556c89522bbfcd62c3e76297
07rihito <onashu.ganbaruzoi@gmail.com>
2021-10-10 01:22:53 +0900

    create db folder

---
+++ db/init/createdatabase.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE app;
+USE app;
+
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    passwd VARCHAR(255)
+);
+
+INSERT INTO users(id, username, email, passwd) VALUES(1, 'sample','sample@sample.com', 'sample');
+INSERT INTO users(id, username, email, passwd) VALUES(2, 'test','test@test.com', 'test');
+INSERT INTO users(id, username, email, passwd) VALUES(3, 'app','app@app.com', 'app');
+
+
+-- CREATE TABLE foodlist(
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     eatdate DATE ,
+--     foodname VARCHAR(255),
+--     fee INT,
+--     store VARCHAR(255)
+-- );
+
+-- ALTER TABLE foodlist ADD store VARCHAR(255);
+
+-- INSERT INTO foodlist(date, name, fee, store) VALUES('2021-04-06', 'スパチキセット', 500, 'マクドナルド');
