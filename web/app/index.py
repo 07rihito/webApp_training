cf70196294c3f95cf064ac325cf63580f197106b
07rihito <onashu.ganbaruzoi@gmail.com>
2021-10-26 20:53:08 +0900

    [modify] add 404 error handling

--- web/app/index.py
+++ web/app/index.py
@@ -7,7 +7,7 @@ from flask import abort
 # from urllib.parse import urlparse
 import mysql.connector
 
-from dbModels import Users
+#from dbModels import Users
 from application import app
 from application import db
 
@@ -34,7 +34,8 @@ def about():
 @app.route('/userList')
 def userlist():
   try:
-    users = Users.query.all()
+    #users = Users.query.all()
+    users = db.executeQuery()
     props = {'title': 'userList'}
     # TODO db接続テスト用に登録user一覧ページを作成する．将来的には管理者しか見れないように
     return render_template('userList.html', users=users, props=props)
@@ -44,36 +45,39 @@ def userlist():
     return render_template('errorPage.html', props=props)
 
 
-# add user operation
-@app.route('/addUser', methods=['POST'])
-def addUser():
-  newUser = request.form['user']
-  newMail = request.form['mail']
-  newPass = request.form['pass']
-  new_user = Users(username=newUser, email=newMail, passwd=newPass)
-  try:
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect(url_for('userlist')) # defで定義されている関数へリダイレクトする
-  except Exception as e:
-    props = {'title': 'error'}
-    props['errorMsg'] = e
-    return render_template('errorPage.html', props=props)
+## add user operation
+#@app.route('/addUser', methods=['POST'])
+#def addUser():
+#  newUser = request.form['user']
+#  newMail = request.form['mail']
+#  newPass = request.form['pass']
+#  new_user = Users(username=newUser, email=newMail, passwd=newPass)
+#  try:
+#    db.session.add(new_user)
+#    db.session.commit()
+#    return redirect(url_for('userlist')) # defで定義されている関数へリダイレクトする
+#  except Exception as e:
+#    props = {'title': 'error'}
+#    props['errorMsg'] = e
+#    return render_template('errorPage.html', props=props)
+#
+#
+## delete user operation
+#@app.route('/delUser/<int:id>')
+#def delUser(id):
+#  deleteUser = Users.query.filter_by(id=id).first()
+#  try:
+#    db.session.delete(deleteUser)
+#    db.session.commit()
+#    return redirect(url_for('userlist'))
+#  except Exception as e:
+#    props = {'title': 'error'}
+#    props['errorMsg'] = e
+#    return render_template('errorPage.html', props=props)
 
-
-# delete user operation
-@app.route('/delUser/<int:id>')
-def delUser(id):
-  deleteUser = Users.query.filter_by(id=id).first()
-  try:
-    db.session.delete(deleteUser)
-    db.session.commit()
-    return redirect(url_for('userlist'))
-  except Exception as e:
-    props = {'title': 'error'}
-    props['errorMsg'] = e
-    return render_template('errorPage.html', props=props)
-
+@app.errorhandler(404)
+def page_not_found(error):
+  return "error occurred: " + error
 
 
 if __name__ == '__main__':
